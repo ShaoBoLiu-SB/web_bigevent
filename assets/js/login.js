3ca81443f57a7dba3d852c7d297351127849cd22
saobo <www.2632901235@qq.com>
2022-05-15 11:13:56 +0800

    完成了登录和注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,29 +1,30 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
+$(function () {
+  // 点击“注册账号”的链接
+  $('#link_reg').on('click', function () {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
+
   // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
+  $('#link_login').on('click', function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
 
-  // 从 layui 中获取 form 对象
+  //从layui中获取form对象
   var form = layui.form
   var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  //通过 from.verify()函数自定义校验规则
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
+    //自定义密码的校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
+    //校验两次密码是否一致的规则
+    repwd: function (value) {
+      //通过形参拿到的是确认密码框中的内容
+      //还需要拿到密码框中的内容
+      //然后进行一次等于判断
+      //如果判断失败，则return一个错误提示
       var pwd = $('.reg-box [name=password]').val()
       if (pwd !== value) {
         return '两次密码不一致！'
@@ -31,23 +32,24 @@ $(function() {
     }
   })
 
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
+  //监听注册表单的提交事件
+  $('#form_reg').on('submit', function (e) {
+    // 1.阻止默认的提交行为
+    e.preventDefault();
+    // 3.把要提交的用户数据抽取出来
+    var data = { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }
+    //2. 发起ajax的POST请求
+    $.post('http://www.liulongbin.top:3007/api/reguser', data,
+      function (res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        layer.msg(res.message)
+        //模拟人的点击行为
+        setTimeout(() => {
+          $('#link_login').click()
+        }, 1000)
+      })
   })
 
   // 监听登录表单的提交事件
@@ -55,20 +57,22 @@ $(function() {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
-      url: '/api/login',
-      method: 'POST',
+      url:'http://www.liulongbin.top:3007/api/login',
+      method:'POST',
       // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
+      data:$(this).serialize(),
+      success(res) {
+        if(res.status !== 0) {
           return layer.msg('登录失败！')
         }
         layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        //将登录成功得到的 token字符串，保存到localStorage中
         localStorage.setItem('token', res.token)
+        console.log(res.token);
         // 跳转到后台主页
-        location.href = '/index.html'
+        // location.href = '/index.html'
       }
     })
   })
+
 })
